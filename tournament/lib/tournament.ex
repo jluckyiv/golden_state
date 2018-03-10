d50cb88f1516eee9134c14ab87b04927667566fa
Jack Lucky <jack.lucky.iv@gmail.com>
2018-03-10 15:48:13 -0800

    Add tournament app

---
+++ tournament/lib/tournament.ex
@@ -0,0 +1,18 @@
+defmodule Tournament do
+  @moduledoc """
+  Documentation for Tournament.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tournament.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ tournament/test/tournament_test.exs
@@ -0,0 +1,8 @@
+defmodule TournamentTest do
+  use ExUnit.Case
+  doctest Tournament
+
+  test "greets the world" do
+    assert Tournament.hello() == :world
+  end
+end
